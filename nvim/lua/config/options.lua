5cc935a90ec414306e427931a56ffb1163c73d16
Gabriel de Oliveira <gabrielhk3@gmail.com>
2025-05-25 18:13:56 -0300

    nvim

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Spellcheck languages
+-- vim.opt.spelllang = { "en", "pt-BR" }
+
+-- Disables LazyVim auto format.
+vim.g.autoformat = false
+
+-- LSP auto detection can be annoying on messy projects.
+vim.g.root_spec = { { ".git", "lua" }, "cwd" }
