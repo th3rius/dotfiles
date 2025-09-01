c7c17c8b380a7f23d8a77f572a36d60ce7539fc7
Gabriel de Oliveira <gabrielhk3@gmail.com>
2025-09-01 14:58:24 -0300

    update neovim

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,14 @@
 return {
-  "catppuccin/nvim",
-  opts = {
-    background = {
-      light = "latte",
-      dark = "frappe",
-    },
+  {
+    "catppuccin",
+    optional = true,
+    opts = function(_, opts)
+      local bufferline = require("catppuccin.groups.integrations.bufferline")
+      bufferline.get = bufferline.get or bufferline.get_theme
+      opts.background = {
+        light = "latte",
+        -- dark = "frappe",
+      }
+    end,
   },
 }
