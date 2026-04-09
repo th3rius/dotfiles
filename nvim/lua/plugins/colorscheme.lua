4001ca712ae52fca74fc1ff45dc6b15723ce33c5
Gabriel de Oliveira <gabrielhk3@gmail.com>
2026-04-09 13:51:02 -0300

    explain colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -8,6 +8,11 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
+      -- I am not sure why but tokyonight seems to work properly properly only
+      -- after explicit setting it as the colorscheme (it should be the default
+      -- colorscheme). I have started noticing this and other issues after
+      -- upgrading to Tahoe 26.4 but I am not sur it is related. We can remove
+      -- this and fall back to the default value if this gets fixed.
       colorscheme = "tokyonight",
     },
   },
